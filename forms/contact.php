d11ad43be148a751f0ad1c6ea94551e0bfceb9b9
Bushra Aboubida Ahmed <bushragtv@gmail.com>
2022-05-22 18:55:11 +0400

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,46 +1,15 @@
 <?php
+if(isset($_POST['submit'])) {
+$name = $_POST['name'];
+$message= $_POST['message'];
 
-  // The receiving email address 
-  $receiving_email_address = 'bushragtv@gmail.com';
+$mailTo = "bushragtv@gmail.com";
+$headers = "From: ".$mailForm;
+$txt = "You have a mesg from your portfolio from ".$name.$message;
 
-  // Check if the file 'php-email-form.php' is exists
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    // include file 'php-email-form.php'
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+mail($mailTo, $name, $txt, $headers);
 
-  // Create a model from the file 'php-email-form.php'
-  $contact = new PHP_Email_Form;
-  // Access the contact methods ajax
-  $contact->ajax = true;
-  // Access the contact variable 'to'
-  $contact->to = $receiving_email_address;
-  // Access the contact variable 'form_name'
-  $contact->from_name = $_POST['name'];
-  // Access the contact variable 'form_email'
-  $contact->from_email = $_POST['email'];
-  // Access the contact variable 'subject'
-  $contact->subject = $_POST['subject'];
+header("Location: index.html?MessageSend");
+}
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  // Access the contact methods add_message and pass POST method value to it and the target
-  $contact->add_message( $_POST['name'], 'From');
-  // Access the contact methods add_message and pass POST method value to it and the target
-  $contact->add_message( $_POST['email'], 'Email');
-  // Access the contact methods add_message and pass POST method value to it and the target
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  // Display send method value
-  echo $contact->send();
 ?>
