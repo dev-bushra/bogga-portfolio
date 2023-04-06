0f2d7fe7033858b055a4874c5f8ca9729327a60e
Bushra Aboubida <58691127+dev-bushra@users.noreply.github.com>
2023-04-07 01:52:15 +0400

    shortcuts

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,12 +75,15 @@
    * Back to top button
    */
   let backtotop = select('.back-to-top')
+  let backtotopResume = select('.back-to-top-resume')
   if (backtotop) {
     const toggleBacktotop = () => {
       if (window.scrollY > 100) {
         backtotop.classList.add('active')
+        backtotopResume.classList.add('active')
       } else {
         backtotop.classList.remove('active')
+        backtotopResume.classList.remove('active')
       }
     }
     window.addEventListener('load', toggleBacktotop)
